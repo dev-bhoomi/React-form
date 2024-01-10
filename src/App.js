087da6bdfbb7924aa960cf57b2f5355dcb7f796b
dev-bhoomi <bhoomigupta912536@gmail.com>
2024-01-10 23:49:09 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { useState } from 'react'
 import './App.css'
 
-const App=()=>{
-  const [name,setName] = useState();
-  const [fullName,setFullName] = useState();
-  const inputEvent=(event)=>{
-  setName(event.target.value)
- }
-  const hello =()=>{
-    setFullName(name)
+const App = () => {
+  const [fullName, setFullName] = useState({
+    fname: '',
+    lname: '',
+  });
+
+  const inputEvent = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFullName((prev) => {
+      if (name === "fname") {
+        return {
+          fname: value,
+          lname: prev.lname,
+        }
+      } else if (name === "lname") {
+        return {
+          fname: prev.fname,
+          lname: value,
+        }
+      }
+    })
   }
-return(
-  <>
-  <h1>Hello {fullName}</h1>
-  <input type="text" 
-  placeholder='Enter Your Name'
-  onChange={inputEvent}
-  value={name}
-  />
-  <button onClick={hello}>Click Me</button>
-  </>
-)
+  const onSubmits = (event) => {
+    event.preventDefault()
+    alert('form submited')
+  }
+  return (
+    <>
+      <div className="main_div">
+        <form onSubmit={onSubmits}></form>
+        <h1>Hello {fullName.fname} {fullName.lname} </h1>
+        <input type="text"
+          placeholder='Enter Your Name'
+          name='fname'
+          onChange={inputEvent}
+          value={fullName.fname} />
+
+        <input type="password"
+          placeholder='Enter Your password'
+          name='lname'
+          onChange={inputEvent}
+          value={fullName.lname}
+        />
+        <button type='submit'>Click Me</button>
+      </div>
+
+    </>
+  )
 }
 
 export default App;
